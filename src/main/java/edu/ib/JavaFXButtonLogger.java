c7073f1deef9cbfc0bce0ce397c9bb96b4765c4a
trywi <249954@student.pwr.edu.pl>
2020-12-10 13:38:16 +0100

    Commit

--- src/main/java/edu/ib/JavaFXButtonLogger.java
+++ src/main/java/edu/ib/JavaFXButtonLogger.java
@@ -1,10 +1,27 @@
 package edu.ib;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class JavaFXButtonLogger {
+import java.io.IOException;
 
-    @FXML
-    void initialize() {
+public class JavaFXButtonLogger extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/calculator.fxml"));
+        Scene scene= new Scene(root,300,400);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
