33ffa393385158ce78531d7505ec9e5b6505b275
trywi <249954@student.pwr.edu.pl>
2020-12-10 14:02:02 +0100

    Commit3

--- src/main/java/edu/ib/JavaFXButtonLoggerController.java
+++ src/main/java/edu/ib/JavaFXButtonLoggerController.java
@@ -1,4 +1,39 @@
 package edu.ib;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class JavaFXButtonLoggerController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btn1;
+
+    @FXML
+    private Button btn2;
+
+    @FXML
+    private TextArea taDisplay;
+
+    @FXML
+    void onButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btn1 != null : "fx:id=\"btn1\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert btn2 != null : "fx:id=\"btn2\" was not injected: check your FXML file 'calculator.fxml'.";
+        assert taDisplay != null : "fx:id=\"taDisplay\" was not injected: check your FXML file 'calculator.fxml'.";
+
+    }
 }
